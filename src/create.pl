aab19728af562d105bfee6ea4f84777a69eab52b
darkclown <darkclown@life-hack.org>
2012-12-12 18:01:57 +0900

    updated table creator to reflect new table layout

--- src/create.pl
+++ src/create.pl
@@ -6,7 +6,7 @@ use Thrift::BinaryProtocol;
 use lib <./HiveConn>;
 use ThriftHive;
 
-my $socket = Thrift::Socket->new("h-apps1.local", 10000);
+my $socket = Thrift::Socket->new("h-apps1.t-lab.cs.teu.ac.jp", 10000);
 $socket->setSendTimeout(600 * 1000); # 10min.
 $socket->setRecvTimeout(600 * 1000);
 my $transport = Thrift::BufferedTransport->new($socket);
@@ -16,15 +16,11 @@ my $client = ThriftHiveClient->new($protocol);
 
 eval {
         $transport->open();
-        $client->execute("CREATE TABLE `twitter_tweet` ( `id` string , `mess` string, `hash` string ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
-        $client->execute("CREATE TABLE `twitter_word_index` ( `id` string , `key_word` string ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
-        $client->execute("CREATE TABLE `twitter_word_count` ( `key_word` string , `cnt` int ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
-        $client->execute("CREATE TABLE `twitter_pair_count` ( `markovone` string , `markovtwo` string , `cnt` int ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile");
-        $client->execute("CREATE TABLE `twitter_pair_index` ( `id` string , `markovone` string , `markovtwo` string ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
-        $client->execute("CREATE TABLE `twitter_hash_index` ( `id` string , `hash` string ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
-        $client->execute("CREATE TABLE `twitter_hash_count` ( `hash` string , `cnt` int ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
+        $client->execute("CREATE TABLE `twitter_tweet` ( `id` string , `mess` string, `hash` string, `rtcnt` INT ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
+        $client->execute("CREATE TABLE `twitter_word_index` ( `key_word` string , `cnt` INT, `id` ARRAY<STRING>, `rtcnt` INT ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' COLLECTION ITEMS TERMINATED BY ',' LINES TERMINATED BY '\n' STORED AS TextFile ");
+        $client->execute("CREATE TABLE `twitter_pattern_index` ( `first` string , `second` string , `third` string, `cnt` INT, `id` ARRAY<STRING>, `rtcnt` INT ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' COLLECTION ITEMS TERMINATED BY ',' LINES TERMINATED BY '\n' STORED AS TextFile ");
+        $client->execute("CREATE TABLE `twitter_hash_index` ( `hash` string, `cnt` INT, `id` ARRAY<STRING>, `rtcnt` INT ) PARTITIONED BY ( `yymmdd` bigint ) ROW FORMAT DELIMITED FIELDS TERMINATED BY COLLECTION ITEMS TERMINATED BY ',' '\t' LINES TERMINATED BY '\n' STORED AS TextFile ");
         
         
         $transport->close();
 };
-if ($@) { print $@->{message}, "\n"; };
